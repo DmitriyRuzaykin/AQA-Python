f2bf010410ae1f9a97739dd91597248c102e7dbb
DmitriyRuzaykin <ruzaykin.dim@yandex.ru>
2025-12-26 19:06:00 +0300

    добавил провекру 2

--- tests/tests.py
+++ tests/tests.py
@@ -24,6 +24,27 @@ def test_1():
     login_button.click()
 
     assert driver.current_url == "https://www.saucedemo.com/inventory.html", "Проверка 1 не прошла"
-    print("Проверка успешно пройдена")
+    print("Проверка 1 успешно пройдена")
+
+def test_2():
+    USERNAME_LOCATOR = ("xpath", "//input[@id='user-name']")
+    PASSWORD_LOCATOR = ("xpath", "//input[@id='password']")
+    LOGIN_LOCATOR = ("xpath", "//input[@id='login-button']")
+
+    driver.get("https://www.saucedemo.com/")
+
+    username = wait.until(EC.visibility_of_element_located(USERNAME_LOCATOR))
+    password = wait.until(EC.visibility_of_element_located(PASSWORD_LOCATOR))
+    login_button = wait.until(EC.element_to_be_clickable(LOGIN_LOCATOR))
+
+    username.send_keys("standard-user")
+    password.send_keys("secret_sauce")
+    login_button.click()
+
+    assert driver.find_element("xpath", "//h3[text()='Epic sadface: Username and password do not match any user in this service']"), "Проверка 2 не прошла"
+    print("Проверка 2 успешно пройдена")
+
 
 test_1()
+test_2()
+
